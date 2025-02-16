41abf75c3a06aba0e006c447d96ef64d0d1b04e5
meotom1198 <81725891+meotom1198@users.noreply.github.com>
2025-02-16 11:52:42 +0700

    Add files via upload

---
+++ var/Typecho/Db/Query/Exception.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Typecho\Db\Query;
+
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+    exit;
+}
+
+use Typecho\Db\Exception as DbException;
+
+/**
+ * Lớp ngoại lệ truy vấn cơ sở dữ liệu
+ *
+ * @package Db
+ */
+class Exception extends DbException
+{
+}
